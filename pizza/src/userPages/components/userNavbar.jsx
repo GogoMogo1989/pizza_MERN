8bb66c82eb96733fa8600250817cf3e9aa9a3989
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-11-15 20:58:55 +0100

    fixxed useNavbar

--- pizza/src/userPages/components/userNavbar.jsx
+++ pizza/src/userPages/components/userNavbar.jsx
@@ -15,6 +15,39 @@ const UserNavbar = () => {
 
   const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
 
+  const menuOptions = {
+    '/basket': [
+      { label: 'Főoldal', onClick: () => navigate('/usermain') },
+      { label: 'Étlap', onClick: () => navigate('/menu') }
+    ],
+    '/menu': [
+      { label: 'Főoldal', onClick: () => navigate('/usermain') },
+      { label: 'Pizzák', to: 'pizza' },
+      { label: 'Saláták', to: 'salad' },
+      { label: 'Deszertek', to: 'dessert' },
+      { label: 'Italok', to: 'drink' }
+    ],
+    '/orderdone': [
+      { label: 'Főoldal', onClick: () => navigate('/usermain') }
+    ],
+    '/user': [
+      { label: 'Főoldal', onClick: () => navigate('/usermain') },
+      { label: 'Étlap', onClick: () => navigate('/menu') }
+    ],
+    '/registration': [
+      { label: 'Főoldal', onClick: () => navigate('/usermain') },
+      { label: 'Étlap', onClick: () => navigate('/menu') }
+    ],
+    'default': [
+      { label: 'Főoldal', to: 'landing' },
+      { label: 'Rólunk', to: 'about-us' },
+      { label: 'Éttermeink', to: 'restaurants' },
+      { label: 'Étlap', onClick: () => navigate('/menu') }
+    ]
+  };
+
+  const currentMenu = menuOptions[location.pathname] || menuOptions['default'];
+
   return (
     <div className="flex justify-between items-center p-2 bg-black bg-opacity-90 text-white h-14 fixed top-0 left-0 right-0 z-50">
       <div>
@@ -22,102 +55,41 @@ const UserNavbar = () => {
       </div>
 
       <div className="hidden md:flex gap-8">
-        {location.pathname === '/basket' ? (
-          <>
+        {currentMenu.map((item, index) => (
+          item.to ? (
             <Link
-              onClick={() => navigate('/usermain')}
+              key={index}
+              to={item.to}
+              smooth={true}
+              duration={500}
               className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer"
             >
-              Főoldal
+              {item.label}
             </Link>
-            <Link 
-              onClick={() => navigate('/menu')}
-              className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Étlap
-            </Link>
-          </>
-        ) 
-
-        : location.pathname === '/menu' ? (
-          <>
-            <Link
-              onClick={() => navigate('/usermain')}
+          ) : (
+            <span
+              key={index}
+              onClick={item.onClick}
               className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer"
             >
-              Főoldal
-            </Link>
-            <Link to="pizza" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Pizzák
-            </Link>
-            <Link to="salad" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Saláták
-            </Link>
-            <Link to="dessert" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Deszertek
-            </Link>
-            <Link to="drink" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Italok
-            </Link>
-          </>
-        ) 
-
-        : location.pathname === '/orderdone' ? (
-          <>
-            <Link
-              onClick={() => navigate('/usermain')}
-              className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer"
-            >
-              Főoldal
-            </Link>
-          </>
-        ) 
-
-        : location.pathname === '/user' ||  location.pathname === '/registration' ? (
-          <>
-           <Link
-              onClick={() => navigate('/usermain')}
-              className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer"
-            >
-              Főoldal
-            </Link>
-            <Link onClick={() => navigate('/menu')} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Étlap
-            </Link>
-          </>
-        )
-
-        : (
-          <>
-            <Link to="landing" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Főoldal
-            </Link>
-            <Link to="about-us" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Rólunk
-            </Link>
-            <Link to="restaurants" smooth={true} duration={500} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Éttermeink
-            </Link>
-            <Link onClick={() => navigate('/menu')} className="text-white hover:text-yellow-300 transition duration-200 cursor-pointer">
-              Étlap
-            </Link>
-          </>
-        )}
+              {item.label}
+            </span>
+          )
+        ))}
       </div>
 
       <div className="flex gap-4 items-center relative">
-        <Link to="basket" smooth={true} duration={500}>
-          <button
-            className="bg-transparent border-none cursor-pointer text-white hover:text-yellow-300 transition duration-200 relative flex items-center justify-center"
-            onClick={() => navigate('/basket')}
-          >
-            <FaShoppingCart size={20} />
-            {cartItemCount > 0 && (
-              <span className="absolute top-[-8px] right-[-8px] bg-red-500 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
-                {cartItemCount}
-              </span>
-            )}
-          </button>
-        </Link>
+        <button
+          className="bg-transparent border-none cursor-pointer text-white hover:text-yellow-300 transition duration-200 relative flex items-center justify-center"
+          onClick={() => navigate('/basket')}
+        >
+          <FaShoppingCart size={20} />
+          {cartItemCount > 0 && (
+            <span className="absolute top-[-8px] right-[-8px] bg-red-500 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
+              {cartItemCount}
+            </span>
+          )}
+        </button>
         <button
           className="bg-transparent border-none cursor-pointer text-white hover:text-yellow-300 transition duration-200"
           onClick={() => navigate('/user')}
@@ -131,114 +103,22 @@ const UserNavbar = () => {
       </div>
 
       <ul className={nav ? 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center text-white' : 'hidden'}>
-        <div className="flex flex-col gap-8">
-          {location.pathname === '/basket' ? (
-            <>
+        {currentMenu.map((item, index) => (
+          <li key={index} className="py-6 text-4xl">
+            {item.to ? (
               <Link
-                onClick={() => navigate('/usermain')}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Főoldal
-              </Link>
-              <Link 
-                onClick={() => navigate('/menu')}
-                className="py-6 text-4xl" smooth={true} duration={500}
-                >
-                Étlap
-              </Link>
-            </>
-          ) 
-
-          : location.pathname === '/menu' ? (
-            <>
-              <Link
-                onClick={() => navigate('/usermain')}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Főoldal
-              </Link>
-              <Link to="pizza" 
+                to={item.to}
+                smooth={true}
+                duration={500}
                 onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
               >
-                Pizzák
+                {item.label}
               </Link>
-              <Link to="salad"
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Salad
-              </Link>
-              <Link to="dessert" 
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Deszertek
-              </Link>
-              <Link to="drink" 
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Italok
-              </Link>
-            </>
-          ) 
-
-          : location.pathname === '/orderdone' ? (
-            <>
-              <Link
-                onClick={() => navigate('/usermain')}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Főoldal
-              </Link>
-            </>
-          ) 
-
-          : location.pathname === '/user' ||  location.pathname === '/registration' ? (
-            <>
-            <Link
-                onClick={() => navigate('/usermain')}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Főoldal
-              </Link>
-              <Link onClick={() => navigate('/menu')} 
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Étlap
-              </Link>
-            </>
-          )
-
-          : (
-            <>
-              <Link to="landing" 
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Főoldal
-              </Link>
-              <Link to="about-us" 
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Rólunk
-              </Link>
-              <Link to="restaurants" 
-                onClick={handleClick}
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Éttermeink
-              </Link>
-              <Link onClick={() => navigate('/menu')} 
-                className="py-6 text-4xl" smooth={true} duration={500}
-              >
-                Étlap
-              </Link>
-            </>
-          )}
-        </div>
+            ) : (
+              <span onClick={() => { item.onClick(); handleClick(); }}>{item.label}</span>
+            )}
+          </li>
+        ))}
       </ul>
     </div>     
   );
